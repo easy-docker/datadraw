a54c41e43bdc3f9a2f070a3215e4920bbd31c079
ghostry <ghostry@ghostry.cn>
2019-04-23 13:01:21 +0800

    add restart

--- create.sh
+++ create.sh
@@ -3,9 +3,9 @@
 mkdir -p tmp
 mkdir -p ../mysqldata
 docker run --rm --interactive --tty -v $PWD:/app -v $PWD/tmp:/tmp --user $(id -u):$(id -g) composer install
-docker run -d -p 3306:3306 --name mysql5 -e MYSQL_ROOT_PASSWORD=123456  -v $PWD/../mysqldata:/var/lib/mysql mysql:5
+docker run -d -p 3306:3306 --name mysql5 -e MYSQL_ROOT_PASSWORD=123456  -v $PWD/../mysqldata:/var/lib/mysql --restart always mysql:5
 docker build . -t php7.2apache
-docker run -d -p 8081:80 --name apache-php-app -v "$PWD":/var/www/html php7.2apache
+docker run -d -p 8081:80 --name apache-php-app -v "$PWD":/var/www/html --restart always php7.2apache
 echo "现在需要创建数据库 datadraw 并把 mysql.sql 导入mysql5"
 echo "mysql用户名root,密码123456"
 echo "修改 application/database.php 中的数据库IP"
---
+++ restart.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker restart apache-php-app mysql5
