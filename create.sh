d7ed66dac66066b0194e96a4662f088e7aa77899
ghostry <ghostry@ghostry.cn>
2019-04-23 13:47:08 +0800

    add importsql

--- create.sh
+++ create.sh
@@ -6,7 +6,7 @@ docker run --rm --interactive --tty -v $PWD:/app -v $PWD/tmp:/tmp --user $(id -u
 docker run -d -p 3306:3306 --name mysql5 -e MYSQL_ROOT_PASSWORD=123456  -v $PWD/../mysqldata:/var/lib/mysql --restart always mysql:5
 docker build . -t php7.2apache
 docker run -d -p 8081:80 --name apache-php-app -v "$PWD":/var/www/html --restart always php7.2apache
-echo "现在需要创建数据库 datadraw 并把 mysql.sql 导入mysql5"
+echo "如果没有旧数据库,现在运行 ./importSQL.sh 导入"
 echo "mysql用户名root,密码123456"
 echo "修改 application/database.php 中的数据库IP"
 echo "然后 docker restart apache-php-app"
---
+++ importSQL.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+USER="root"
+PASSWORD="123456"
+DATABASE="datadraw"
+SQLSTR=`cat mysql.sql`
+######################
+#crate database
+docker exec -it mysql5 mysql -u $USER -p$PASSWORD -e "CREATE DATABASE $DATABASE;"
+#importdata
+docker exec -it mysql5 mysql -u $USER -p$PASSWORD $DATABASE -e $SQLSTR
