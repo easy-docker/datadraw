8e01468bed7c32bf2cf2e639bf63ddb0715cdd2d
ghostry <ghostry@ghostry.cn>
2019-04-23 14:33:31 +0800

    fix

--- importSQL.sh
+++ importSQL.sh
@@ -7,4 +7,4 @@ SQLSTR=`cat mysql.sql`
 #crate database
 docker exec -it mysql5 mysql -u $USER -p$PASSWORD -e "CREATE DATABASE $DATABASE;"
 #importdata
-docker exec -it mysql5 mysql -u $USER -p$PASSWORD $DATABASE -e $SQLSTR
+docker exec -it mysql5 mysql -u $USER -p$PASSWORD $DATABASE -e "$SQLSTR"
